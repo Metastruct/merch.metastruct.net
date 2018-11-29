30b0d739208157f8f0355885f6094f74823b667d
KeyLimePie <v.k.asakura@gmail.com>
2018-11-29 18:27:09 +0100

    Don't use animation on carousel controllers.

--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,8 @@
 $(function() {
 	// Smooth scrolling using jQuery easing
 	$('a[href*="#"]:not([href="#"])').click(function() {
+		if ($(this).parent().is(".carousel"))
+			return;
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 		var target = $(this.hash);
 		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
